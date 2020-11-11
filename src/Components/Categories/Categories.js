f617d418e0c78cdf74dbbdb14165c15a7d63d2be
Joao <joaoricardotg@gmail.com>
2020-11-11 13:13:21 -0300

    Finished the categories component
    - the categories component is composed of the elements immediately below the hero carousel

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -3,6 +3,7 @@ import styles from './Categories.module.css'
 import background from './img/background'
 
 const Categories = () => {
+   
     return (
         <div className={styles.categories}>
             <div className={styles.item}>
@@ -10,20 +11,21 @@ const Categories = () => {
                 <img src={background} className={styles.background}></img>
                 <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus mauris id magna fermentum euismod.</p>
                 <h1 className={styles.h1}>CATEGORIA</h1>
-                <div className={styles.overlay1}></div>
-                <div className={styles.overlay2}></div>
+                <div className={styles.overlay} ></div>
             </div>
             <div className={styles.item}>
                 <h2 className={styles.headLine}>Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
                 <img src={background} className={styles.background}></img>
                 <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus mauris id magna fermentum euismod.</p>
                 <h1 className={styles.h1}>CATEGORIA</h1>
+                <div className={styles.overlay} ></div>
             </div>
             <div className={styles.item}>
                 <h2 className={styles.headLine}>Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
                 <img src={background} className={styles.background}></img>
                 <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus mauris id magna fermentum euismod.</p>
                 <h1 className={styles.h1}>CATEGORIA</h1>
+                <div className={styles.overlay} ></div>
             </div>
         </div>
     )
